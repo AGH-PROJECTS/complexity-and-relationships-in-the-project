476bd062e0e7c3a200ffe9c70446583f81381c10
Rozamer <dawid.kruczek.1997@gmail.com>
2019-10-16 16:32:51 +0200

    Update .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+package main.java;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -5,14 +7,12 @@ import java.util.Set;
 import java.util.Map.Entry;
 
 public class Main {
+    public static final String ROOT_PATH = System.getProperty("user.dir");// scieżka, w której program szuka plików
     public static void main(String[] args) {
-
-        String rootPath = "C:\\src";            // scieżka, w której program szuka plików
-
         try {
 
             FileVisitor fileVisitor = new FileVisitor();
-            fileVisitor.findFiles(rootPath);
+            fileVisitor.findFiles(ROOT_PATH);
             Map<String, Map<String, Integer>> fileUsageMap = new HashMap<>();
             fileUsageMap = fileVisitor.searchFiles();
             printUsageMap(fileUsageMap);
