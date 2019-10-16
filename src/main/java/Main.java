4c2642d145011ee10c66eb037bd5ab4c58b9dcc3
smolentomasz <tomaszsmolen97@gmail.com>
2019-10-16 20:59:33 +0200

    change drawing class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,33 +6,19 @@ import java.util.Map.Entry;
 
 public class Main {
     public static final String ROOT_PATH = System.getProperty("user.dir");// scieżka, w której program szuka plików
+    private static Map<String, Map<String, Integer>> fileUsageMap = new HashMap<>();
     public static void main(String[] args) {
-        JGraphXDraw graph1 = new JGraphXDraw();
-        graph1.createGraphX();
         try {
-
-            main.java.FileVisitor fileVisitor = new main.java.FileVisitor();
+            FileVisitor fileVisitor = new FileVisitor();
             fileVisitor.findFiles(ROOT_PATH);
-            Map<String, Map<String, Integer>> fileUsageMap = new HashMap<>();
             fileUsageMap = fileVisitor.searchFiles();
-            printUsageMap(fileUsageMap);
+
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+        JGraphXDraw.createGraphX(fileUsageMap);
 
     }
 
-    private static void printUsageMap(Map<String, Map<String, Integer>> fileUsageMap) {
-
-        Set<Entry<String, Map<String, Integer>>> entrySet = fileUsageMap.entrySet();
-        for(Entry<String, Map<String, Integer>> entry: entrySet){
-            System.out.println(entry.getKey());
-            Set<Entry<String, Integer>> littleEntrySet = entry.getValue().entrySet();
-            for(Entry<String, Integer> littleEntry: littleEntrySet){
-                System.out.println("\t" + littleEntry.getKey() + ": " + littleEntry.getValue());
-            }
-        }
-
-    }
 }
