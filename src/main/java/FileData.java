66beef8582723806c8c117ba4434404973f12f85
Przemyslaw98 <48160770+Przemyslaw98@users.noreply.github.com>
2019-10-16 20:46:52 +0200

    Update FileData.java

--- src/main/java/FileData.java
+++ src/main/java/FileData.java
@@ -3,10 +3,10 @@ import java.util.Map;
 
 public class FileData {
     private String name;                                    //nazwa
-    private long size;                                   //rozmiar
+    private Long size;                                   //rozmiar
     private Map<String, Map<String, Integer>> dependencies; //mapa połączeń i ich wagi
 
-    public FileData(String n,long i,Map<String,Map<String,Integer>> m){
+    public FileData(String n,Long i,Map<String,Map<String,Integer>> m){
         name=n;
         size=i;
         dependencies=new HashMap<String,Map<String,Integer>>(m);
