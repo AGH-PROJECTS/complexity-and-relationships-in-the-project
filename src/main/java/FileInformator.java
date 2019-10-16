28809b85e356eee35dfb3b4d3e5aef4f43ae1760
Rozamer <dawid.kruczek.1997@gmail.com>
2019-10-16 19:08:11 +0200

    Map<String,Long> zawiera informacje o ilosci linii pliku + drobne zmiany kosmetyczne

--- src/main/java/FileInformator.java
+++ src/main/java/FileInformator.java
@@ -1,4 +1,58 @@
-package main.java;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class FileInformator {
+    private String path;
+    Map<String,Long> infoAboutFile = new HashMap<>();
+
+    public FileInformator(String path) {
+        this.path = path;
+    }
+
+    public Map<String, Long> getInformation() {
+
+
+        File projectDirectory = new File(path);
+
+        List<File> filesInProjectDir = Arrays.asList(projectDirectory.listFiles());
+
+        List<File> sourceFiles = filesInProjectDir.stream()
+                .filter(file -> file.toString().endsWith(".java"))
+                .collect(Collectors.toList());
+
+        filesInProjectDir.stream()
+                .filter(
+                        file -> file.isDirectory())
+                .forEach(dir -> {
+                            List<File> sourceFilesInDir = Arrays.asList(dir.listFiles()).stream()
+                                    .filter(file -> file.toString().endsWith(".java"))
+                                    .collect(Collectors.toList());
+                            sourceFilesInDir.forEach(file -> sourceFiles.add(file));
+                        });
+
+        sourceFiles.forEach(e-> {
+            try {
+                long lines = 0;
+                Scanner scanner = new Scanner(e);
+                while (scanner.hasNextLine()) {
+                    scanner.nextLine();
+                    lines++;
+                }
+                infoAboutFile.put(e.getName(),lines);
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        showInformation();
+        return infoAboutFile;
+    }
+
+    private void showInformation() {
+        infoAboutFile.entrySet()
+                .forEach(file -> System.out.println("Name: " + file.getKey() +", size: " + file.getValue()));
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,26 @@
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.Map.Entry;
 
 public class Main {
-    public static final String ROOT_PATH = System.getProperty("user.dir");// scieżka, w której program szuka plików
+    public static final String ROOT_PATH = System.getProperty("user.dir")+"\\src\\main\\java";// scieżka, w której program szuka plików
     public static void main(String[] args) {
         JGraphXDraw graph1 = new JGraphXDraw();
         graph1.createGraphX();
+        FileInformator fileInformator = new FileInformator(ROOT_PATH);
+        Map<String,Long> filesInformation = fileInformator.getInformation();
         try {
-
-            main.java.FileVisitor fileVisitor = new main.java.FileVisitor();
+            FileVisitor fileVisitor = new FileVisitor();
             fileVisitor.findFiles(ROOT_PATH);
             Map<String, Map<String, Integer>> fileUsageMap = new HashMap<>();
             fileUsageMap = fileVisitor.searchFiles();
             printUsageMap(fileUsageMap);
             
-            List<FileData> fileDataList=new LinkedList<>();
+            /*List<FileData> fileDataList=new LinkedList<>();
             Set<Entry<String, Map<String, Integer>>> entrySet = fileUsageMap.entrySet();
             for(Entry<String, Map<String, Integer>> entry: entrySet){
                 fileDataList.add(new FileData(entry.getKey(),fileSize,entry.getValue());  //fileSize do dodania
-            }
+            }*/
             
 
         } catch (IOException e) {
