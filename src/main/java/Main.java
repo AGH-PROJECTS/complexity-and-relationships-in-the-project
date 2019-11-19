259adc910b2d659f1f32e7abeb241a9d059ea2e7
smolentomasz <tomaszsmolen97@gmail.com>
2019-11-19 21:09:29 +0100

    przebudowanie klasy rysującej grafy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,8 @@ import model.JGraphXDraw;
 public class Main {
     public static final String ROOT_PATH = System.getProperty("user.dir")+"\\src\\main\\java";// scieżka, w której program szuka plików
     private static Map<String, Map<String, Integer>> fileUsageMap = new HashMap<>();
+    private static Map<String, Map<String, Integer>>  methods;
+    private static Map<String, Map<String, Integer>> packages;
     public static void main(String[] args) {
         FileInformator fileInformator = new FileInformator(ROOT_PATH);
         Map<String,Long> filesInformation = fileInformator.getInformation();
@@ -20,9 +22,9 @@ public class Main {
             fileVisitor.findFiles(ROOT_PATH);
             fileUsageMap = fileVisitor.searchFiles();
             InformationGenerator informationGenerator = new InformationGenerator();
-            InformationGenerator informationGenerator1 = new InformationGenerator();
-            Map<String, Map<String, Integer>>  methods =  informationGenerator.getInformationMethods();
-            Map<String, Map<String, Integer>> packages = informationGenerator.getInformationPackages();
+           // InformationGenerator informationGenerator1 = new InformationGenerator();
+            methods =  informationGenerator.getInformationMethods();
+            packages = informationGenerator.getInformationPackages();
             //informationGenerator1.getInformationMethods();
             //int [][]graph = new int[fileVisitor.getNameList().size()][fileVisitor.getNameList().size()];
 
@@ -32,77 +34,10 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(packages);
+        JGraphXDraw myGraph = new JGraphXDraw();
+        myGraph.createGraphX(packages, filesInformation);
+        //JGraphXDraw.createGraphX(fileUsageMap, filesInformation);
         //JGraphXDraw.createGraphX(fileUsageMap, filesInformation);
     }
-
-    private static int [][]graph (List<String> nameList)
-    {
-        int listsize = nameList.size();
-        int connections[][] = new int[listsize][listsize]; // tworzymy tablice ktora okresili nam kierunki polaczen
-        for(int g = 0; g <listsize; g++)
-        {
-            for(int h = 0; h < listsize; h++)
-            {
-                connections[g][h] = -1;
-            }
-        }
-        String rootPath = "C:\\Users\\KuroiAkuma\\Desktop\\projectIO-faza_1\\projectIO-faza_1\\src\\main\\java"; //moja sciezka dla folderu projektu
-        String filepath;
-        int index;
-
-        for(int i = 0; i < listsize; i++)
-        {
-            index = 0;
-            filepath = rootPath + "\\" + nameList.get(i) + ".java"; // ustalamy sciezke pliku w ktorym sprawdzamy polaczenia
-            //System.out.println(filepath + "\n");
-            File file = new File(filepath);
-            try {
-                Scanner scanner = new Scanner(file);
-                String line;
-                while (scanner.hasNextLine()) {
-                    line = scanner.nextLine();
-                    for (int j = 0; j < listsize; j++) {
-                        //System.out.println(j);
-                        if (line.contains(nameList.get(j)) && i != j) {
-                            //System.out.println(i + "\t" + j);
-                            connections[i][j] = j;
-                            index++;
-                            break;
-                        }
-                        /*else
-                        {
-                            connections[i][j] = -1;
-                            //index++;
-                        }*/
-                    }
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-        return connections;
-
-    }
-
-    private static void printList(int [][] graph, List<String> nameList)
-    {
-        /*
-        reprezentacja kierunkowości połączeń w grafie
-        element graph[i][j] jest potrzebny elementowi i
-        i -----> graph[i][j]
-        */
-
-        for(int i = 0; i < nameList.size(); i++)
-        {
-            for(int j = 0; j < nameList.size(); j++)
-            {
-                if(graph[i][j] >= 0)
-                    //System.out.println(i + " " + j + " " +  graph[i][j]);
-                    System.out.println(nameList.get(i) + " -----> " + nameList.get(j));
-            }
-
-        }
-
-    }
-
 }
--- src/main/java/model/JGraphXDraw.java
+++ src/main/java/model/JGraphXDraw.java
@@ -5,86 +5,187 @@ import com.mxgraph.model.mxCell;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JApplet;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-//Main
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
 public class JGraphXDraw extends JApplet {
     private static Map<String, Map<String, Integer>> fileUsageMap;
     private static Map<String,Long> filesInformation;
     private static final Dimension DEFAULT_SIZE = new Dimension(530, 320);
     private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private static JFrame frame = new JFrame();
+    private static mxGraph myGraph;
+    private static Object parent;
+    private mxCircleLayout layout;
 
-    public static void createGraphX(Map<String, Map<String, Integer>> filesMap, Map<String,Long> filesInfo) {
+    public void createGraphX(Map<String, Map<String, Integer>> filesMap, Map<String, Long> filesInfo) {
         JGraphXDraw applet = new JGraphXDraw();
         fileUsageMap = filesMap;
         filesInformation = filesInfo;
         applet.init();
 
-        JFrame frame = new JFrame();
-        frame.getContentPane().add(applet);
+        JPanel allContent = new JPanel(new BorderLayout());
+
+
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+
+
+        JButton btn1 = new JButton("Historia 1");
+        btn1.addActionListener(e ->  {
+            myGraph.getModel().beginUpdate();
+            history1Graph(myGraph, parent);
+            myGraph.getModel().endUpdate();
+
+            SwingUtilities.updateComponentTreeUI(frame);
+        });
+       // btn1.setPreferredSize(new Dimension(50,50));
+        panel.add(btn1,BorderLayout.PAGE_START);
+
+        JButton btn2 = new JButton("Historia 2");
+        btn2.addActionListener(e ->  {
+            myGraph.getModel().beginUpdate();
+            history2Graph(myGraph, parent);
+            myGraph.getModel().endUpdate();
+            SwingUtilities.updateComponentTreeUI(frame);
+        });
+       // btn2.setPreferredSize(new Dimension(50,50));
+        panel.add(btn2,BorderLayout.CENTER);
+
+        JButton btn3 = new JButton("Historia 3");
+        btn3.addActionListener(e ->  {
+            myGraph.getModel().beginUpdate();
+            history2Graph(myGraph, parent);
+            myGraph.getModel().endUpdate();
+            SwingUtilities.updateComponentTreeUI(frame);
+        });
+       // btn3.setPreferredSize(new Dimension(50,50));
+        panel.add(btn3,BorderLayout.LINE_END);
+
+        allContent.add(panel, BorderLayout.PAGE_START);
+        allContent.add(applet,BorderLayout.CENTER);
+
+        frame = new JFrame();
         frame.setTitle("Inżynieria oprogramowania - rozpoczęcie projektu");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(screenSize.width, screenSize.height);
         frame.setResizable(true);
+        frame.add(allContent);
+        frame.setContentPane(allContent);
         frame.setVisible(true);
+
     }
 
     @Override
     public void init() {
-        mxGraph graf = new mxGraph();
+        myGraph = new mxGraph();
 
-        Object parent = graf.getDefaultParent();
+        parent = myGraph.getDefaultParent();
 
+        mxGraphComponent graphComponent = new mxGraphComponent(myGraph);
+
+        getContentPane().add(graphComponent);
+
+    }
+    private void history1Graph(mxGraph myGraph, Object myParent){
         ArrayList<mxCell> vertexList = new ArrayList<>();
         ArrayList<mxCell> vertexList2;
 
+        Set<Map.Entry<String, Map<String, Integer>>> entrySet = fileUsageMap.entrySet();
+        for (Map.Entry<String, Map<String, Integer>> entry : entrySet) {
+            long size = filesInformation.get(entry.getKey() + ".java");
+            vertexList.add((mxCell) myGraph.insertVertex(myParent, null, entry.getKey(), 0, 0, size, size));
+        }
+        vertexList2 = vertexList;
+        for (Map.Entry<String, Map<String, Integer>> entry : entrySet) {
+            Set<Map.Entry<String, Integer>> littleEntrySet = entry.getValue().entrySet();
+            for (Map.Entry<String, Integer> littleEntry : littleEntrySet) {
 
-        graf.getModel().beginUpdate();
-        try {
-            Set<Map.Entry<String, Map<String, Integer>>> entrySet = fileUsageMap.entrySet();
-            for (Map.Entry<String, Map<String, Integer>> entry : entrySet) {
-                long size = filesInformation.get(entry.getKey() + ".java");
-                vertexList.add((mxCell) graf.insertVertex(parent, null, entry.getKey(), 0, 0, size*2, size*2));
-            }
-            vertexList2 = vertexList;
-            for (Map.Entry<String, Map<String, Integer>> entry : entrySet) {
-                Set<Map.Entry<String, Integer>> littleEntrySet = entry.getValue().entrySet();
-                for (Map.Entry<String, Integer> littleEntry : littleEntrySet) {
-
-                    if (littleEntry.getValue() > 0) {
-                        for (Object o : vertexList) {
-                            for (Object o2 : vertexList2) {
-                                if (o.toString().contains(entry.getKey())) {
-                                    if (o2.toString().contains(littleEntry.getKey())) {
-                                        graf.insertEdge(parent, null, littleEntry.getValue(), o, o2);
-                                    }
+                if (littleEntry.getValue() > 0) {
+                    for (Object o : vertexList) {
+                        for (Object o2 : vertexList2) {
+                            if (o.toString().contains(entry.getKey())) {
+                                if (o2.toString().contains(littleEntry.getKey())) {
+                                    myGraph.insertEdge(myParent, null, littleEntry.getValue(), o, o2);
                                 }
                             }
                         }
                     }
                 }
             }
-        } finally {
-            graf.getModel().endUpdate();
         }
-        mxGraphComponent graphComponent = new mxGraphComponent(graf);
-        mxCircleLayout layout = new mxCircleLayout(graf);
+        setCircleLayout(myGraph);
+    }
+    private void history2Graph(mxGraph myGraph, Object myParent){
+        ArrayList<mxCell> vertexList = new ArrayList<>();
+        ArrayList<mxCell> vertexList2;
+        ArrayList<String> allNodes = new ArrayList<>();
+        Set<Map.Entry<String, Map<String, Integer>>> entrySet = fileUsageMap.entrySet();
+
+        for (Map.Entry<String, Map<String, Integer>> entry : entrySet) {
+            Set<Map.Entry<String, Integer>> littleEntrySet = entry.getValue().entrySet();
+            allNodes.add(entry.getKey());
+            for(Map.Entry<String, Integer> littleEntry : littleEntrySet){
+                allNodes.add(littleEntry.getKey());
+            }
+        }
+        ArrayList<String> uniqueNodes = new ArrayList<>(new HashSet<>(allNodes));
+
+        for(String unique: uniqueNodes){
+            vertexList.add((mxCell) myGraph.insertVertex(myParent, null, unique, 0, 0, 50, 50));
+        }
+
+
+        vertexList2 = vertexList;
+        for (Map.Entry<String, Map<String, Integer>> entry : entrySet) {
+            Set<Map.Entry<String, Integer>> littleEntrySet = entry.getValue().entrySet();
+            for (Map.Entry<String, Integer> littleEntry : littleEntrySet) {
+
+                if (littleEntry.getValue() > 0) {
+                    for (Object o : vertexList) {
+                        for (Object o2 : vertexList2) {
+                            if (o.toString().contains(entry.getKey())) {
+                                if (o2.toString().contains(littleEntry.getKey())) {
+                                    myGraph.insertEdge(myParent, null, littleEntry.getValue(), o, o2);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        setCircleLayout(myGraph);
+    }
+    private void setCircleLayout(mxGraph myGraph){
+        layout = new mxCircleLayout(myGraph);
         int radius = 100;
-        layout.setX0((DEFAULT_SIZE.width / 2.0) - radius);
-        layout.setY0((DEFAULT_SIZE.height / 2.0) - radius);
+        layout.setX0(DEFAULT_SIZE.width/2);
+        layout.setY0(DEFAULT_SIZE.height/2);
         layout.setRadius(radius);
         layout.setMoveCircle(true);
 
-        layout.execute(graf.getDefaultParent());
-
-        getContentPane().add(graphComponent);
-
+        layout.execute(parent);
     }
-
 }
